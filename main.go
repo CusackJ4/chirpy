5d39b482a7e28ba87d370ecd510c57ff4ffc0656
CusackJ4 <28494505+CusackJ4@users.noreply.github.com>
2026-05-05 16:02:17 -0300

    added hitcounter middleware and associated handler methods

--- main.go
+++ main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
 	const filePathRoot = "."
 	const port = "8080"
+	var apiCfg apiConfig
 
 	mux := http.NewServeMux()
 	// mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
-	mux.Handle("/app/", http.StripPrefix("/app",
-		http.FileServer(http.Dir(filePathRoot))))
+	mux.Handle("/app/",
+		// StripPrefix allows any location with '/app/' to be searchable
+		apiCfg.middlewareMetricsInc(http.StripPrefix("/app",
+			http.FileServer(http.Dir(filePathRoot)))))
 
+	mux.HandleFunc("/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/reset", apiCfg.hitresetHandler)
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
@@ -31,6 +38,28 @@ func main() {
 
 }
 
-// the first compiles the binary and runs, the second runs without compiling
-// go build -o out && ./out
-// go run .
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+// Middleware that is used to track hits to the site
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+// method that's used to print hits to the site
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprint(w, "Hits: ", cfg.fileserverHits.Load())
+}
+
+// method that's used to reset # of hits to the site
+func (cfg *apiConfig) hitresetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Counter reset to 0\n"))
+}
