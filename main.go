3cc0e4adce110907b26f3be7f09c752d2d237c52
CusackJ4 <28494505+CusackJ4@users.noreply.github.com>
2026-05-20 14:12:18 -0300

    message length validation added

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
 	"sync/atomic"
+	"unicode/utf8"
 )
 
 func main() {
@@ -19,13 +21,15 @@ func main() {
 		apiCfg.middlewareMetricsInc(http.StripPrefix("/app",
 			http.FileServer(http.Dir(filePathRoot)))))
 
-	mux.HandleFunc("GET /api/metrics", apiCfg.metricsHandler)
-	mux.HandleFunc("POST /api/reset", apiCfg.hitresetHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.hitresetHandler)
+
 	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(http.StatusText(http.StatusOK)))
 	})
+	mux.HandleFunc("POST /api/validate_chirp", apiCfg.chirplengthHandler)
 
 	//2. Initialize the httpServer field
 	srv := &http.Server{
@@ -42,7 +46,16 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-// Middleware that is used to track hits to the site
+// html template for hit metrics
+var metricsTmpl = template.Must(template.New("metrics").Parse(`<!DOCTYPE html>
+<html>
+<body>
+	<h1>Welcome, Chirpy Admin</h1>
+	<p>Chirpy has been visited {{.Hits}} times!</p>
+</body>
+</html>`))
+
+// Middleware method that is used to track hits to the site
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits.Add(1)
@@ -52,8 +65,17 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 
 // method that's used to print hits to the site
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprint(w, "Hits: ", cfg.fileserverHits.Load())
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	data := struct {
+		Hits int32
+	}{
+		Hits: cfg.fileserverHits.Load(),
+	}
+	// fmt.Fprint(w, "Hits: ", cfg.fileserverHits.Load())
+	if err := metricsTmpl.Execute(w, data); err != nil {
+		http.Error(w, "ERROR: Failed to render template", http.StatusInternalServerError)
+	}
+
 }
 
 // method that's used to reset # of hits to the site
@@ -62,4 +84,55 @@ func (cfg *apiConfig) hitresetHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Counter reset to 0\n"))
+}
+
+// method to accept a POST request and send a response.
+func (cfg *apiConfig) chirplengthHandler(w http.ResponseWriter, r *http.Request) {
+
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	// params now has a populated body (string)
+	bodyLength := utf8.RuneCountInString(params.Body)
+
+	switch {
+	case bodyLength > 140:
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+	case bodyLength <= 140:
+		respondWithJson(w, http.StatusOK, true)
+	}
+}
+
+type APIResponse[T any] struct {
+	Data  T      `json:"data,omitempty"`
+	Error string `json:"error,omitempty"`
+	Valid bool   `json:"valid,omitempty"`
+}
+
+func writeJson(w http.ResponseWriter, code int, v any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	if err := json.NewEncoder(w).Encode(v); err != nil {
+		log.Printf("Failed to encode JSON: %v", err)
+	}
+
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	writeJson(w, code, APIResponse[any]{Error: msg})
+}
+
+// data T is here because it was asked for in the lesson.
+func respondWithJson[T any](w http.ResponseWriter, code int, data T) {
+	writeJson(w, code, APIResponse[T]{Valid: true})
 }
